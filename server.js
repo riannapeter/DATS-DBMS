e03f39b0e6a1e54444846b366cb5c7735aa26515
Rianna Peter <rianna.peter@gmail.com>
2026-05-06 04:40:53 +0530

    fix: use env variables for DB

--- server.js
+++ server.js
@@ -2,19 +2,20 @@
 const express = require("express");
 const path = require("path");
 const mysql = require("mysql2");
+
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "DATSdbms1234",
-  database: "dats",
-  port: 3307
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
   if (err) {
     console.error("❌ DB connection failed:", err);
   } else {
-    console.log("✅ Connected to MySQL");
+    console.log("✅ Connected to Railway MySQL");
   }
 });
 const app = express();
